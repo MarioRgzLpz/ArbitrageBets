1308217131b613d6bf380b941d10501315d6eb1c
adiazcencillo <albertodiazcen@gmail.com>
2024-10-30 12:21:40 +0100

    closes #10 estableciendo la relación entre cuotas y eventos deportivos permitiendo agregar a las casas de apuestas eventos junto a las cuotas correspondientes a sus resultados

--- src/casa_de_apuestas.rs
+++ src/casa_de_apuestas.rs
@@ -13,4 +13,16 @@ impl CasaDeApuestas {
             cuotas: HashMap::new()
         }
     }
+
+    pub fn agregar_evento(&mut self, evento: EventoDeportivo, cuotas: Vec<Cuota>) -> Result<(),String> {
+
+        for cuota in &cuotas {
+            if !evento.get_resultados().contains(&cuota.get_resultado()) {
+                return Err(format!("Resultado no válido '{}' en la cuota.", cuota.get_resultado()));
+            }
+        }
+
+        self.cuotas.insert(evento, cuotas);
+        Ok(())
+    }
 }
