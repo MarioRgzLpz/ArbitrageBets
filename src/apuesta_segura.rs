e3fc239f171b1f26247a25457cbcdda14b843665
adiazcencillo <albertodiazcen@gmail.com>
2024-10-30 12:24:00 +0100

    closes #11 definiendo la estructura ApuestaSegura como un evento y un conjunto de cuotas, también se añade un constructor con limitaciones que permiten la correcta corrspondencia entre los resultados del evento y los resultados de las cuotas

---
+++ src/apuesta_segura.rs
@@ -0,0 +1,20 @@
+use crate::{cuota::Cuota, evento::{self, EventoDeportivo}};
+
+pub struct ApuestaSegura {
+    evento: EventoDeportivo,
+    cuotas: Vec<Cuota>
+}
+
+impl ApuestaSegura {
+    
+    pub fn new(evento: EventoDeportivo, cuotas: Vec<Cuota>) -> Result<Self,String> {
+
+        for cuota in &cuotas {
+            if !evento.get_resultados().contains(&cuota.get_resultado()) {
+                return Err(format!("Resultado no válido '{}' en la cuota.", cuota.get_resultado()));
+            }
+        }
+
+        Ok(ApuestaSegura { evento, cuotas })
+    }
+}
