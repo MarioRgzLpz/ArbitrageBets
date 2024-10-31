fe132d77e952f1f0a129ecec12bdcdf333013123
adiazcencillo <albertodiazcen@gmail.com>
2024-10-31 12:58:25 +0100

    closes #7
    Se elimina la función para agregar eventos ya que se pretende establecer un producto MINIMAMENTE viable, por lo que funciones de este tipo no son necesarias para resolver el problema

--- src/casa_de_apuestas.rs
+++ src/casa_de_apuestas.rs
@@ -14,23 +14,5 @@ impl CasaDeApuestas {
             cuotas: HashMap::new()
         }
     }
-
-    pub fn agregar_evento(&mut self, evento: EventoDeportivo, cuotas: Vec<Cuota>) -> Result<(),String> {
-
-        if cuotas.len() != 3 {
-            return Err(format!("A un evento solo se pueden asociar tres cuotas"));
-        }
-
-        let mut resultados_set = std::collections::HashSet::new();
-        for cuota in &cuotas {
-            if !resultados_set.insert(cuota.get_resultado()) {
-                return Err(format!("No se puede asociar el resultado {:?} más de una vez", cuota.get_resultado()));
-            }
-        }
-
-        let nombre_evento = evento.get_nombre().clone();
-
-        self.cuotas.insert(nombre_evento, cuotas);
-        Ok(())
-    }
+    
 }
