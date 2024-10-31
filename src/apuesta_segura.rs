4c7d564953e497c6465615c1e0439ae6b9914393
adiazcencillo <albertodiazcen@gmail.com>
2024-10-31 13:51:31 +0100

    closes #6
    Añado docstring para aclarar la clave del Map definido en ApuestaSegura

--- src/apuesta_segura.rs
+++ src/apuesta_segura.rs
@@ -3,6 +3,7 @@ use crate::{cuota::Cuota, evento::EventoDeportivo};
 
 pub struct ApuestaSegura {
     evento: String,
+    /// La clave de `cuotas` es un String representando el nombre de la casa de apuestas que ofrece dicha cuota
     cuotas: HashMap<String, Cuota>
 }
 
