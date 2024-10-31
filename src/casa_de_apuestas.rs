f12033401fd203947b352be05c99395fc490f4a5
adiazcencillo <albertodiazcen@gmail.com>
2024-10-31 13:50:57 +0100

    closes #6
    Añado docstring para aclarar la clave del Map definido en CasaDeApuestas

--- src/casa_de_apuestas.rs
+++ src/casa_de_apuestas.rs
@@ -3,6 +3,7 @@ use crate::{cuota::Cuota, evento::EventoDeportivo};
 
 pub struct CasaDeApuestas {
     nombre: String,
+    /// La clave de `cuotas` es un String representando el nombre del evento al cual queremos asociar las cuotas
     cuotas: HashMap<String, Vec<Cuota>>
 }
 
