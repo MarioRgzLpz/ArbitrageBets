23ca19058fc0b02a94ade58b18ff579e96175e68
MarioRgzLpz <MarioRgzLpz@correo.ugr.es>
2024-12-05 12:02:03 +0100

    feat: closes #22
    Añado dos getters para poder obtener los atributos de una CasaDeApuestas y poder generar la apuesta segura

--- src/casa_de_apuestas.rs
+++ src/casa_de_apuestas.rs
@@ -1,4 +1,4 @@
-use crate::{cuota::Cuota, evento::EventoDeportivo};
+use crate::cuota::Cuota;
 use std::collections::HashMap;
 
 /// La clave de `cuotas` es un String representando el nombre del evento y el String de `nombre` representa el nombre de la casa de apuestas
@@ -8,10 +8,15 @@ pub struct CasaDeApuestas {
 }
 
 impl CasaDeApuestas {
-    pub fn new(nombre: String) -> Self {
-        CasaDeApuestas {
-            nombre,
-            cuotas: HashMap::new(),
-        }
+    pub fn new(nombre: String, cuotas: HashMap<String, Vec<Cuota>>) -> Self {
+        CasaDeApuestas { nombre, cuotas }
+    }
+
+    pub fn get_nombre(&self) -> &String {
+        &self.nombre
+    }
+
+    pub fn get_cuotas(&self) -> &HashMap<String, Vec<Cuota>> {
+        &self.cuotas
     }
 }
